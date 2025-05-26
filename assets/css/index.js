fcc52ea0b1b872d58f90181096425f4cef2794fe
Rahulsaini-web <Rahulsaini9351@gmail.com>
2025-05-26 19:44:58 +0530

    first commit

--- assets/css/index.js
+++ assets/css/index.js
@@ -24,16 +24,3 @@
         });
     });
 
-    var swiper = new Swiper(".mySwiper", {
-      slidesPerView: 3,
-      spaceBetween: 30,
-      loop: true,
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
